862435a3b9518dcb3f5d72792b8b849ec7a2c4b0
S-Heijnemans <153827872+S-Heijnemans@users.noreply.github.com>
2025-06-05 15:46:23 +0200

    add skelet album

---
+++ Spotivy/Classes/Album.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotivy.Classes
+{
+    class Album
+    {
+        private List<Artist> Artists = new List<Artist>();
+
+        /**
+         * this is to create a album
+         */
+        public Album(List<Artist> artists)
+        {
+            Artists = artists;
+        }
+
+        /**
+         * this is to show the artists of the album
+         */
+        public List<Artist> ShowArtist()
+        {
+            return new List<Artist>();
+        }
+        /**
+         * this is to change how strings are printed to inprove clarity
+         */
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
