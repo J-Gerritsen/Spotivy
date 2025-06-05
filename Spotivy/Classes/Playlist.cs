d3aa698194a1462f4bd135c16cacf47bfa045d4c
S-Heijnemans <153827872+S-Heijnemans@users.noreply.github.com>
2025-06-05 16:06:28 +0200

    add skelet playlist

---
+++ Spotivy/Classes/Playlist.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotivy.Classes
+{
+    class Playlist
+    {
+        public Person Owner;
+
+        /**
+         * this is to create a Playlist
+         */
+        public Playlist(Person owner)
+        {
+            Owner = owner;
+        }
+
+        /**
+         * ??
+         */
+        public void Add(iPlayable iPlayable)
+        {
+
+        }
+
+        /**
+         * ??
+         */
+        public void Remove(iPlayable iPlayable)
+        {
+
+        }
+
+        /**
+        * this is to change how strings are printed to improve clarity
+        */
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
