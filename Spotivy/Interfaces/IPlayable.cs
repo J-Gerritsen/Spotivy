ab6bae6a4df2896383669125d78d22ff5fe374bc
job-gerritsen <154206850+J-Gerritsen@users.noreply.github.com>
2025-06-05 16:47:36 +0200

    Add skelet for Client, SuperUser, Person and iPlayables.

---
+++ Spotivy/Interfaces/IPlayable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotivy.Interfaces
+{
+    interface iPlayable
+    {
+        void Play();
+        void Pause();
+        void Next();
+        void Stop();
+        int Length { get; }
+    }
+}
