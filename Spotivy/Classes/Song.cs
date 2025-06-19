0925ed448a0116460024543b6b6a75ba6ee5fef2
S-Heijnemans <153827872+S-Heijnemans@users.noreply.github.com>
2025-06-19 15:49:40 +0200

    add so the user can see what song is playing

--- Spotivy/Classes/Song.cs
+++ Spotivy/Classes/Song.cs
@@ -45,6 +45,7 @@ namespace Spotivy.Classes
             while (Playing)
             {
                 Playing = true;
+                Console.WriteLine("currently playing: " + Title);
                 Thread.Sleep(1000); //wait for 1 second
                 i++;
 
